8a84742604372389a1ee0296fab5bcdaf46ef5ab
Jane Saldana <janesaldana@Janes-Mac-mini.local>
2023-04-12 23:00:27 -0500

    added form and header

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 //create welcome pop up message
 alert(    "Welcome to my personal webpage! Thanks for visiting!")
+
+const form = document.querySelector("#form")
+   const submitButton = document.querySelector("#submit")
+   const scriptURL = 'https://script.google.com/macros/s/AKfycbxLZTlgHBunzjDAyvzTFB9AAQZS2vGT4hSZKlfMDVMQrrHEQwKDYO2-fo05BNfFegYWVg/exec'
+
+   form.addEventListener('submit', e => {
+     submitButton.disabled = true
+     e.preventDefault()
+     let requestBody = new FormData(form)
+     fetch(scriptURL, { method: 'POST', body: requestBody})
+       .then(response => {
+          alert('Success!', response)
+          submitButton.disabled = false
+         })
+       .catch(error => {
+       alert('Error!', error.message)
+         submitButton.disabled = false
+
+       }
+       )
+   })
